70549a81369454a5ab6c519ac812d194e9632549
andre <andrey.karmanov.2011@yandex.ru>
2018-12-24 02:18:14 +0500

    Telegram working bot

--- src/com/quizbot/telegram/TelegramBot.java
+++ src/com/quizbot/telegram/TelegramBot.java
@@ -1,4 +1,56 @@
 package com.quizbot.telegram;
 
-public class TelegramBot {
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+public class TelegramBot extends TelegramLongPollingBot {
+    private static String botUsername;
+    private static String botToken;
+
+    TelegramBot(String username, String token) {
+        botUsername = username;
+        botToken = token;
+
+    }
+    @Override
+    public void onUpdateReceived(Update e) {
+        Message msg = e.getMessage(); // Это нам понадобится
+        String txt = msg.getText();
+        if (txt.equals("/start")) {
+            sendMsg(msg, "Hello, world! This is simple bot!");
+        }
+    }
+
+    private void sendMsg(Message msg, String text) {
+        SendMessage s = new SendMessage();
+        s.setChatId(msg.getChatId()); // Боту может писать не один человек, и поэтому чтобы отправить сообщение, грубо говоря нужно узнать куда его отправлять
+        s.setText(text);
+        try { //Чтобы не крашнулась программа при вылете Exception
+            execute(s);
+        } catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Метод возвращает имя бота, указанное при регистрации.
+     * @return имя бота
+     */
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    /**
+     * Метод возвращает token бота для связи с сервером Telegram
+     * @return token для бота
+     */
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
 }
